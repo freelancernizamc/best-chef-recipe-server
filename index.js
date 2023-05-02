961c0144c83a79559699d92d1d245e6b06502cab
freelancernizamc <nizamuddin2020ctg@gmail.com>
2023-05-02 13:12:37 +0600

    cors installed and data loaded

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+const chefs = require('./data/chefs.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Chef receipe is running')
 });
+
+app.get('/chefs', (req, res) => {
+    console.log(chefs);
+    res.send(chefs);
+})
 
 app.listen(port, () => {
     console.log(`Chef recipe API is running on port: ${port}`)
