aff73d0690d588e08025187654484d1948bba174
freelancernizamc <nizamuddin2020ctg@gmail.com>
2023-05-03 17:23:48 +0600

    server deployed in vercel

--- index.js
+++ index.js
@@ -15,6 +15,17 @@ app.get('/chefs', (req, res) => {
     console.log(chefs);
     res.send(chefs);
 })
+app.get('/chefs/:id', (req, res) => {
+    const id = req.params.id;
+    const chef = chefs.find(c => c.id == id);
+    if (chef) {
+        res.send(chef);
+    } else {
+        res.status(404).send({ error: 'Chef not found' });
+    }
+});
+
+
 
 app.listen(port, () => {
     console.log(`Chef recipe API is running on port: ${port}`)
